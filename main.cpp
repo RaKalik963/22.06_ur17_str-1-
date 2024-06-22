d6551071679bab171cbc2893c3d823956a03b19d
RaKalik963 <andrejmahot646@gmail.com>
2024-06-22 16:49:37 +0300

    dz-ex1

--- main.cpp
+++ main.cpp
@@ -2,10 +2,26 @@
 using namespace std;
 
 
-
+void replace(char* str, int num)
+{
+	for (size_t i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			str[num] = ' ';
+		}
+	}
+}
 
 int main()
 {
+	char str[] = "hello world!";
+	int num;
+	cout << "enter num to delete symbol";
+	cin >> num;
+
+	replace(str, num);
+	cout << str << endl;
 
 	system("pause");
 	return 0;

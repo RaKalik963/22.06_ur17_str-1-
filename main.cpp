95a5d92e946d938196691bcdeeae434e8fb1f37c
RaKalik963 <andrejmahot646@gmail.com>
2024-06-22 17:30:43 +0300

    34324

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 using namespace std;
 
+void removeAllChars(char str[], char symbol) {
+    int len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
 
+    int j = 0;
+    for (int i = 0; i < len; i++) {
+        if (str[i] != ' ') {
+            str[symbol] = str[i];
+        }
+    }
+    str[j] = '\0';
+}
 
+int main() {
+    char str[] = "hello world hello world";
+    char symbol;
 
-int main()
-{
+    cout << "Enter symbol to paste: ";
+    cin >> symbol;
 
-	system("pause");
-	return 0;
+    removeAllChars(str, symbol);
+
+    cout << "Result: " << str << endl;
+
+    return 0;
 }

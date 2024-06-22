8ea7032bf2f0f0669cfe95ccd2a6ec87839fe30a
RaKalik963 <andrejmahot646@gmail.com>
2024-06-22 16:16:30 +0300

    lab-ex1

--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 using namespace std;
 
 
-
+void replace(char* str)
+{
+	for (size_t i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			str[i] = '\t';
+		}
+	}
+}
 
 int main()
 {
+	char str[] = "hello world! hello world! hello world!";
+
+	replace(str);
+	cout << str << endl;
 
 	system("pause");
 	return 0;

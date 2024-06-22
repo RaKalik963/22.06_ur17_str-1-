795e754d7a889c65583391ef67cea879a3b6bc2e
RaKalik963 <andrejmahot646@gmail.com>
2024-06-22 17:06:02 +0300

    2423

--- main.cpp
+++ main.cpp
@@ -2,10 +2,33 @@
 using namespace std;
 
 
-
+void replace(char* str, int num)
+{
+	for (size_t i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			str[char(num)] = ' ';
+		}
+	}
+}
 
 int main()
 {
+	for (size_t i = 65; i <= 122; i++)
+	{
+		cout << "\t\t" << i << " - " << char(i) << endl;
+	}
+	char str[] = "Hello world!";
+	int num;
+	do
+	{
+		cout << "enter num to delete symbol";
+		cin >> num;
+	} while (num <= 65 && num >= 122);
+
+	replace(str, num);
+	cout << str << endl;
 
 	system("pause");
 	return 0;

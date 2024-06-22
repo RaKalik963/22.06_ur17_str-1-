849d403a5be94f46f5d4ffc6875c374defedc452
RaKalik963 <andrejmahot646@gmail.com>
2024-06-22 16:22:36 +0300

    lab-ex2

--- main.cpp
+++ main.cpp
@@ -2,11 +2,18 @@
 using namespace std;
 
 
-
+size_t my_strlen(const char* str) {
+    size_t len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
 
 int main()
 {
-
+	char str[] = "hello world!";
+    cout << "amount of symbols in str = " << my_strlen(str) << endl;
 	system("pause");
 	return 0;
 }

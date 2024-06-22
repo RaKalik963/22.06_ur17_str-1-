bcf9ac68425057edd5bcf7450f341a62821ac9f5
RaKalik963 <andrejmahot646@gmail.com>
2024-06-22 17:15:43 +0300

    lab-ex3

--- main.cpp
+++ main.cpp
@@ -3,9 +3,25 @@ using namespace std;
 
 
 
+int main() {
+    char str[] = "hello world hello world hello world hello world";
 
-int main()
-{
+    int word_count = 0;
+    bool in_word = false;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n') {
+            in_word = false;
+        }
+        else {
+            if (!in_word) {
+                word_count++;
+                in_word = true;
+            }
+        }
+    }
+
+    cout << "Amount of words = " << word_count << endl;
 
 	system("pause");
 	return 0;

ec61239a83a9243e5c24df02a2e0809278e1ab4c
RaKalik963 <andrejmahot646@gmail.com>
2024-06-30 19:39:29 +0300

    dz-ex4

--- main.cpp
+++ main.cpp
@@ -1,38 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void removeAllChars(char str[]) {
-    int len = 0;
-    while (str[len] != '\0') {
-        len++;
-    }
-
-    int j = 0;
-    for (int i = 0; i < len; i++) {
-        if (str[symbol] == num) {
-            str[symbol] = str[num];
-        }
-    }
-    str[j] = '\0';
-}
-void replace(char* str, int num)
-{
-    for (size_t i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] == ' ')
-        {
-            str[num] = ' ';
+void replace_dots(char* str) {
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (str[i] == '.') {
+            str[i] = '!';
         }
     }
 }
 
 int main() {
-    char str[100];
+    char str[100]; 
+    cout << "Enter str: ";
+    cin.getline(str, 100);
 
-    cout << "enter position to paste your symbol";
-    cin >> str;
-
-    removeAllChars(str);
+    replace_dots(str);
 
     cout << "Result: " << str << endl;
 

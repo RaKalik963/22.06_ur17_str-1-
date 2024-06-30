1e82776d10b35b5f6b6cae936eaff847e17a8b72
RaKalik963 <andrejmahot646@gmail.com>
2024-06-30 19:43:54 +0300

    dz-ex5

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int countOccurrences(const char* str, char symbol) {
+    int count = 0;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (str[i] == symbol) {
+            ++count;
+        }
+    }
+    return count;
+}
 
+int main() {
+    char str[100]; 
+    cout << "Enter str: ";
+    cin.getline(str, 100);
 
+    char symbol;
+    cout << "Enter symbol to find ";
+    cin >> symbol;
 
-int main()
-{
+    int count = countOccurrences(str, symbol);
 
-	system("pause");
-	return 0;
+    cout << "Result " << count << endl;
+
+    return 0;
 }

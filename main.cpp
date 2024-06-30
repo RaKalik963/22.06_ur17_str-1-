645cb1c56f67e1af6842549c108a300b4328b6a4
RaKalik963 <andrejmahot646@gmail.com>
2024-06-30 19:17:57 +0300

    lab-ex4

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 using namespace std;
 
+void palidrom(char* str) {
+	int len = 0;
+	for (int i = 0; str[i] != '\0'; ++i) {
+		len++;
+	}
 
+	bool isPalidrom = true;
 
+	for (int i = 0; i < len / 2; i++) {
+		if (str[i] != str[len - 1 - i]) {
+			isPalidrom = false;
+			break;
+		}
+	}
 
-int main()
-{
+	if (isPalidrom) {
+		cout << "The string is a palindrome." << endl;
+	}
+	else {
+		cout << "The string is not a palindrome." << endl;
+	}
+}
+int main() {
+	char str[200];
+	cout << "enter str ";
+	cin.getline(str, 200);
+	palidrom(str);
 
-	system("pause");
 	return 0;
 }
